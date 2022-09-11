bae9dce3541e7af6a9bd11088c3b63e4983dd97e
Mariya0810 <112906665+Mariya0810@users.noreply.github.com>
2022-09-11 11:47:10 +0200

    Update javascript index

---
+++ index.js
@@ -0,0 +1,41 @@
+let searchEngine = document.querySelector("#search-form");
+searchEngine.addEventListener("submit", handleSubmit);
+
+function showTemp(response) {
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+function search(city) {
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchValue = document.querySelector(".form-control");
+  let city = searchValue.value;
+  search(city);
+}
+
+search("Sofia");
+
+function searchLocation(position) {
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function getLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getLocation);
